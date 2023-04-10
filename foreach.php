40c98209e7fc7c0561770f42aabfca186e9a7770
LETICIA ARAUJO PEREIRA <leticia.1970@aluno.pr.senac.br>
2023-04-10 08:00:22 -0300

    salvar

---
+++ foreach.php
@@ -0,0 +1,40 @@
+<?php
+$b1 = array ();
+
+$b1 [0][0] = "Nome:";
+$b1 [0][1] = "Letícia";
+
+$b1[1][0] = "Idade";
+$b1[1][1] = "16";
+
+$b1[2][0] = "Sexo";
+$b1[2][1] = "Feminino";
+
+$b1[3][0] = "Estado Civil";
+$b1[3][1] = "Solteira";
+
+$b1[4][0] = "Profissão";
+$b1[4][1] = "Consultora de vendas";
+
+$b1[5][0] = "Cidade";
+$b1[5][1] = "Ivaiporã";
+
+$b1 [0][0] = "Bairro";
+$b1 [0][1] = "Centro";
+
+$b1[1][0] = "Rua";
+$b1[1][1] = "Santa Catarina";
+
+$b1[2][0] = "Número";
+$b1[2][1] = "AP 7";
+
+$b1[3][0] = "CEP";
+$b1[3][1] = "86870000";
+
+$b1[4][0] = "Telefone";
+$b1[4][1] = "34721361";
+
+$b1[5][0] = "hobbie";
+$b1[5][1] = "jogar";
+
+?>
